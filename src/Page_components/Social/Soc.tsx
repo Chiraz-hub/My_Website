cea38ec87f4c5eef9996e4a22d14e886fa4aec92
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-20 12:29:11 +0100

    worked on the socials file

---
+++ src/Page_components/Social/Soc.tsx
@@ -0,0 +1,21 @@
+function Socials(){
+    return(
+     <div className = 'socials-container'>
+        <h1>
+            Contact Me
+        </h1>
+        <div className = 'media'>
+          <div className = 'git'>
+            <button className = 'gith'>GitHub</button>
+          </div>
+          <div className = 'linked'>
+            <button className = 'lin'>LinkedIn</button>
+          </div>
+          <div className = 'leet'>
+           <button className = 'leetc'>LeetCode</button>
+          </div>
+        </div>
+     </div>
+    );
+};
+export default Socials;
