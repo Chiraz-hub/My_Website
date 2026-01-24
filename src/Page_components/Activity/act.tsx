760efd021be1f424cd3dce2f58661a11e574a934
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-24 09:44:43 +0100

    Rendered the Icons in there places

--- src/Page_components/Activity/act.tsx
+++ src/Page_components/Activity/act.tsx
@@ -1,28 +1,32 @@
 import Cards from '../Formats/Card'
+import SecBtn from './ActIcon/SecBtn'
+import Math from './ActIcon/Math'
+import PrblmS from './ActIcon/PrblmS'
+import Soft from './ActIcon/Soft'
   const Act = [
     {
      id: 1,
      title: 'Networking & Cybersecurity',
      description: 'I work on computer networking fundamentals and cybersecurity concepts, focusing on how systems communicate, detect vulnerabilities, and stay secure.',
-     logo: 'nothing for now'
+     logo: <SecBtn />
     },
     {
      id: 2,
      title: 'Algorithmic Problem Solving',
      description: 'I enjoy solving algorithmic problems and translating real-world problems into structured pseudocode and efficient logic.',
-     logo: 'nothing for now'
+     logo: <PrblmS />
     },
     {
      id: 3,
      title: 'Software Specializations',
      description: 'I design and build software solutions by combining web technologies, algorithmic thinking, and low-level system knowledge to create efficient, reliable, and well-structured applications.',
-     logo:'nothing for now'
+     logo: <Soft />
     },
     {
       id: 4,
       title: 'Mathematical & Analytical Thinking',
       description: 'I apply mathematical and statistical concepts to strengthen logical reasoning and problem-solving skills.',
-      logo: 'nothing for now'
+      logo: <Math />
     }
   ];
 function Activity(){
@@ -32,10 +36,10 @@ function Activity(){
          <div className = 'card-container'>
         {Act.map((item, index) => (
           <Cards 
-            key={index} 
+            logo={item.logo}  
             title={item.title} 
             description={item.description} 
-            logo={item.logo} 
+            key={index} 
           />
         ))}
          </div>
