1caf6d8c59ca829a83689a366a17e4c5afd55b13
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-24 09:34:16 +0100

    Restructured the intro folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Greating from "./Page_components/Intro_structure/Title";
-import Nav from "./Page_components/Intro_structure/NavBar";
+import Greating from "./Page_components/Intro_structure/Title/Title";
+import Nav from "./Page_components/Intro_structure/NavBar/NavBar";
 import Desc from "./Page_components/About_Structure/Description"
 import Activity from "./Page_components/Activity/act"
 import Skill from "./Page_components/SkillSet/SkillDes"
 import Hire from "./Page_components/HireMe/hire"
-import SecBtn from './Page_components/Activity/ActIcon/SecBtn'
-import PrblmS from './Page_components/Activity/ActIcon/PrblmS'
-import Soft from './Page_components/Activity/ActIcon/Soft'
-import Math from './Page_components/Activity/ActIcon/Math'
 function App(){
  return (
      <div>
@@ -15,10 +11,6 @@ function App(){
     <Greating />
     <Desc />
     <Activity />
-    <SecBtn />
-    <PrblmS />
-    <Soft />
-    <Math />
     <Skill/>
     <Hire />
     </div>
