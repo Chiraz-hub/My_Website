4e294a8bc9f9b124da47c585f5bb3af059202fd0
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-26 23:23:31 +0100

    CSS work added

--- src/Page_components/Intro_structure/NavBar/NavBar.tsx
+++ src/Page_components/Intro_structure/NavBar/NavBar.tsx
@@ -2,13 +2,13 @@ import './NavBar.css'
 function Nav(){
     return (
       <nav>
-        <ul>
-            <li className="about">Home</li>
-            <li className="about">About</li>
-            <li className="about">TechStack</li>
-            <li className="about">Socials</li>
+        <ul> 
+            <li><button className = 'abt-btn'>Home</button></li>
+            <li><button className = 'abt-btn'>About</button></li>
+            <li><button className = 'abt-btn'>TechStack</button></li>
+            <li><button className = 'abt-btn'>Socials</button></li>
         </ul>
       </nav>
     );
-}
+};
 export default Nav;
