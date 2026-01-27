21e172d937a0a8aaca33379c0922e9e24ec7695d
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-27 15:27:00 +0100

    Added the Who am i core css structur

--- src/Page_components/About_Structure/Description.tsx
+++ src/Page_components/About_Structure/Description.tsx
@@ -3,10 +3,10 @@ function Desc(){
     return(
         <div className="description-container">
          <div className ='text-description'>
-             <h1>
+             <h1 className = 'whoami'>
         WHO AM I ?
      </h1> 
-     <p>
+     <p className ='textdes'>
         Boucenna Chiraz a motivated and detail-oriented developer with a strong foundation in algorithms, JavaScript, CSS, and modern web technologies.
         I am passionate about problem-solving and continuous learning, enjoying building clean, efficient solutions and exploring new concepts in programming and data analysis.
      </p>
