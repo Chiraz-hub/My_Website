9de5047626b86b5973114749299ca95b8a239282
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-01-28 17:09:28 +0100

    structured the skill part

--- src/Page_components/SkillSet/SkillDes.tsx
+++ src/Page_components/SkillSet/SkillDes.tsx
@@ -1,10 +1,11 @@
+ import './SkillSet.css'
  import Loader from './SkillLod'
 function Skill() {
     return (
         <div className = 'container-skill'>
             <div className = 'des-skill'>
-                <h1>SkillSet Description</h1>
-                <p>
+                <h1 className = 'title-skill'>SkillSet Description</h1>
+                <p className = 'des-skill'>
                     software development, networking, and cybersecurity fundamentals, with a strong foundation in algorithms, system-level concepts, 
                     and web technologies. I work with both theoretical and practical tools, and I continuously expand my skills 
                     through hands-on projects, problem-solving, and self-directed learning.</p>
