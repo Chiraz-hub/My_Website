a90f534a1c0b98b35f029baed35341b13563d9fb
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-02-08 20:45:23 +0100

    fixed the intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Greating from "./Page_components/Intro_structure/Title/Title";
-import Nav from "./Page_components/Intro_structure/NavBar/NavBar";
 import Desc from "./Page_components/About_Structure/Description"
 import Activity from "./Page_components/Activity/act"
 import Skill from "./Page_components/SkillSet/SkillDes"
@@ -9,10 +8,7 @@ function App() {
   return (
     <> 
     <main>
-    <section className = 'section'>
-      <Nav />
-      <Greating />
-      </section>
+    <section className = 'section'><Greating /></section>
     <section className= 'section'><Desc /></section>
     <section className = 'section'><Activity /></section>
     <section className = 'section'><Skill /></section>
--- src/Page_components/Intro_structure/Title/Title.tsx
+++ src/Page_components/Intro_structure/Title/Title.tsx
@@ -1,10 +1,21 @@
 import './Title.css';
 function Greating(){
     return (
+      <div className = 'great'>
+          <nav>
+        <ul> 
+            <li><button className = 'abt-btn'>Home</button></li>
+            <li><button className = 'abt-btn'>About</button></li>
+            <li><button className = 'abt-btn'>TechStack</button></li>
+            <li><button className= 'abt-btn'>Skills</button></li>
+            <li><button className = 'abt-btn'>Socials</button></li>
+        </ul>
+      </nav>
       <div className = "intro">
      <h1 className ='gre-h1'>Hey, I Am Boucenna !</h1>
      <p className = 'small-des-gre'>Welcome To My Page. Software & Security </p>
      <button className = 'ScrollBtn'>Scroll Down ↓</button>
+      </div>
       </div>
     );
 }
