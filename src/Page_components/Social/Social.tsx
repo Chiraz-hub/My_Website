315a7d66b719ef28e7a8a07eca09a09c47eaad9e
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-02-12 08:55:30 +0100

    Social designing

--- src/Page_components/Social/Social.tsx
+++ src/Page_components/Social/Social.tsx
@@ -1,10 +1,25 @@
-import './Soc';
-import SocialsIcon from './Soc';
+import SocialIcon from './Soc';
 import './Soc.css'
 function Socials(){
     return(
+        <div className = 'hole-social-container'>
       <div className='social-container'>
-      
+         <div className = 'social-text'>
+            <h1 className = 'social-title'>Contact Me</h1>
+            <p className = 'social-description'>Have a project in mind, a collaboration idea, or just want to connect?  
+Feel free to reach out — I'm always open to meaningful conversations and opportunities.
+</p>
+         </div>
+         <div className = 'social-icon'>
+           <SocialIcon />
+         </div>
+     </div>
+     <div className='Thanks'>
+        <h1 className = 'thanks-title'>THANK YOU !</h1>
+        <footer>
+         <p>&copy; 2026 <span className = 'blue'>Boucenna Chiraz</span>. All rights reserved.</p>
+        </footer>
+     </div>
      </div>
     )
 }
