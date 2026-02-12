879f82440dc14dcbd30b29a28f1ebf7fe0e84779
Chiraz Boucenna <boucennachiraz07@gmail.com>
2026-02-12 10:31:06 +0100

    Time for Responsive Tests

--- src/Page_components/Social/Soc.tsx
+++ src/Page_components/Social/Soc.tsx
@@ -1,155 +1,117 @@
-
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const SocialIcon = () => {
   return (
-    <StyledWrapper>
+    <Wrapper>
       <div className="main">
-        <div className="up">
-          <button className="card1">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="30px" height="30px" fillRule="nonzero" className="instagram"><g fillRule="nonzero" stroke="none" strokeWidth={1} strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit={10} strokeDasharray strokeDashoffset={0} fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{mixBlendMode: 'normal'}}><g transform="scale(8,8)"><path d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z" /></g></g></svg>
+
+        <div className="row">
+          <button className="card instagram">
+            <svg viewBox="0 0 24 24">
+              <path d="M7 2C4.243 2 2 4.243 2 7v10c0 2.757 2.243 5 5 5h10c2.757 0 5-2.243 5-5V7c0-2.757-2.243-5-5-5H7zm0 2h10c1.654 0 3 1.346 3 3v10c0 1.654-1.346 3-3 3H7c-1.654 0-3-1.346-3-3V7c0-1.654 1.346-3 3-3zm11 1a1 1 0 100 2 1 1 0 000-2zM12 7a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6z"/>
+            </svg>
           </button>
-          <button className="card2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="30px" height="30px" className="twitter"><path d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429" /></svg>
-          </button> </div>
-       
-        <div className="down">
-          <button className="card3">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px" className="github">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z" /></svg>
-          </button>
-          <button className="card4">
-            <svg height="30px" width="30px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" className="discord"><path d="M40,12c0,0-4.585-3.588-10-4l-0.488,0.976C34.408,10.174,36.654,11.891,39,14c-4.045-2.065-8.039-4-15-4s-10.955,1.935-15,4c2.346-2.109,5.018-4.015,9.488-5.024L18,8c-5.681,0.537-10,4-10,4s-5.121,7.425-6,22c5.162,5.953,13,6,13,6l1.639-2.185C13.857,36.848,10.715,35.121,8,32c3.238,2.45,8.125,5,16,5s12.762-2.55,16-5c-2.715,3.121-5.857,4.848-8.639,5.815L33,40c0,0,7.838-0.047,13-6C45.121,19.425,40,12,40,12z M17.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C21,28.209,19.433,30,17.5,30z M30.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C34,28.209,32.433,30,30.5,30z" /></svg>
+
+          <button className="card twitter">
+            <svg viewBox="0 0 24 24">
+              <path d="M22 5.92c-.77.35-1.6.58-2.46.69a4.2 4.2 0 001.85-2.3 8.3 8.3 0 01-2.65 1.02 4.15 4.15 0 00-7.07 3.78A11.8 11.8 0 013 4.9a4.14 4.14 0 001.28 5.53 4.1 4.1 0 01-1.88-.52v.05c0 2 1.43 3.67 3.33 4.05a4.2 4.2 0 01-1.87.07 4.16 4.16 0 003.88 2.88A8.34 8.34 0 012 19.54a11.77 11.77 0 006.29 1.84c7.55 0 11.68-6.25 11.68-11.67 0-.18 0-.35-.01-.52A8.2 8.2 0 0022 5.92z"/>
+            </svg>
           </button>
         </div>
+
+        <div className="row">
+          <button className="card github">
+            <svg viewBox="0 0 24 24">
+              <path d="M12 .5A11.5 11.5 0 000 12a11.5 11.5 0 008 10.94c.6.11.82-.26.82-.58v-2.04c-3.34.73-4.04-1.6-4.04-1.6-.55-1.4-1.34-1.77-1.34-1.77-1.1-.75.08-.74.08-.74 1.22.08 1.86 1.25 1.86 1.25 1.08 1.86 2.84 1.32 3.54 1.01.11-.79.42-1.33.76-1.63-2.66-.3-5.46-1.33-5.46-5.92 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.5.12-3.13 0 0 1.01-.32 3.3 1.23a11.4 11.4 0 016 0c2.29-1.55 3.3-1.23 3.3-1.23.66 1.63.24 2.83.12 3.13.77.84 1.24 1.91 1.24 3.22 0 4.6-2.8 5.61-5.47 5.91.43.37.82 1.1.82 2.23v3.3c0 .32.22.7.83.58A11.5 11.5 0 0024 12 11.5 11.5 0 0012 .5z"/>
+            </svg>
+          </button>
+
+          <button className="card discord">
+       <svg viewBox="0 0 24 24" fill="currentColor">
+  <path d="M20.317 4.369A19.791 19.791 0 0015.885 3c-.191.328-.403.775-.553 1.125a18.27 18.27 0 00-6.664 0A11.53 11.53 0 008.115 3a19.736 19.736 0 00-4.432 1.369C1.743 7.16 1.24 9.874 1.5 12.553c1.29.96 2.54 1.545 3.77 1.934.303-.414.572-.855.806-1.32-.444-.168-.867-.376-1.264-.617.106-.08.21-.162.31-.246 2.44 1.14 5.09 1.14 7.502 0 .102.084.205.166.31.246-.398.241-.821.45-1.266.617.236.465.505.906.81 1.32 1.23-.39 2.48-.974 3.77-1.934.32-3.29-.547-5.98-2.233-8.184zM9.75 10.75c-.73 0-1.33-.67-1.33-1.5s.59-1.5 1.33-1.5c.75 0 1.35.67 1.33 1.5 0 .83-.58 1.5-1.33 1.5zm4.5 0c-.73 0-1.33-.67-1.33-1.5s.59-1.5 1.33-1.5c.75 0 1.35.67 1.33 1.5 0 .83-.58 1.5-1.33 1.5z"/>
+</svg>
+
+
+          </button>
+        </div>
+
       </div>
-    </StyledWrapper>
+    </Wrapper>
   );
-}
-
-const StyledWrapper = styled.div`
-  .main {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5em;
-  }
-
-  .up {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5em;
-  }
-
-  .down {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5em;
-  }
-
-  .card1 {
-    width: 90px;
-    height: 90px;
-    outline: none;
-    border: none;
-    background: white;
-    border-radius: 90px 5px 5px 5px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-    transition: .2s ease-in-out;
-  }
-
-  .instagram {
-    margin-top: 1.5em;
-    margin-left: 1.2em;
-    fill: #cc39a4;
-  }
-
-  .card2 {
-    width: 90px;
-    height: 90px;
-    outline: none;
-    border: none;
-    background: white;
-    border-radius: 5px 90px 5px 5px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-    transition: .2s ease-in-out;
-  }
-
-  .twitter {
-    margin-top: 1.5em;
-    margin-left: -.9em;
-    fill: #03A9F4;
-  }
-
-  .card3 {
-    width: 90px;
-    height: 90px;
-    outline: none;
-    border: none;
-    background: white;
-    border-radius: 5px 5px 5px 90px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-    transition: .2s ease-in-out;
-  }
-
-  .github {
-    margin-top: -.6em;
-    margin-left: 1.2em;
-  }
-
-  .card4 {
-    width: 90px;
-    height: 90px;
-    outline: none;
-    border: none;
-    background: white;
-    border-radius: 5px 5px 90px 5px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-    transition: .2s ease-in-out;
-  }
-
-  .discord {
-    margin-top: -.9em;
-    margin-left: -1.2em;
-    fill: #8c9eff;
-  }
-
-  .card1:hover {
-    cursor: pointer;
-    scale: 1.1;
-    background-color: #cc39a4;
-  }
-
-  .card1:hover .instagram {
-    fill: white;
-  }
-
-  .card2:hover {
-    cursor: pointer;
-    scale: 1.1;
-    background-color: #03A9F4;
-  }
-
-  .card2:hover .twitter {
-    fill: white;
-  }
-
-  .card3:hover {
-    cursor: pointer;
-    scale: 1.1;
-    background-color: black;
-  }
-
-  .card3:hover .github {
-    fill: white;
-  }
-
-  .card4:hover {
-    cursor: pointer;
-    scale: 1.1;
-    background-color: #8c9eff;
-  }
-
-  .card4:hover .discord {
-    fill: white;
-  }`;
+};
 
 export default SocialIcon;
+
+const Wrapper = styled.div`
+
+  --size: 100px;
+  --icon: calc(var(--size)/3);
+
+  display:flex;
+  justify-content:center;
+  align-items:center;
+
+  .main{
+    display:flex;
+    flex-direction:column;
+    gap:1.4em;
+    animation: float 4s ease-in-out infinite;
+  }
+
+  .row{
+    display:flex;
+    gap:1.4em;
+  }
+
+  .card{
+    width:var(--size);
+    height:var(--size);
+    border:none;
+    cursor:pointer;
+    border-radius:24px;
+    background:#fff;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    transition:.35s;
+    box-shadow:
+      rgba(0,0,0,0.25) 0px 10px 25px -5px,
+      rgba(0,0,0,0.3) 0px 8px 10px -8px;
+  }
+
+  svg{
+    width:var(--icon);
+    height:var(--icon);
+    transition:.35s;
+  }
+
+  /* colors */
+  .instagram svg{ fill:#cc39a4; }
+  .twitter svg{ fill:#03A9F4; }
+  .github svg{ fill:#000; }
+  .discord svg{ fill:#8c9eff; }
+
+  /* hover effects */
+  .card:hover{
+    transform:scale(1.15) rotate(3deg);
+    box-shadow:0 0 35px rgba(0,255,255,0.6);
+  }
+
+  .card:hover svg{
+    fill:white;
+  }
+
+  .instagram:hover{ background:#cc39a4; }
+  .twitter:hover{ background:#03A9F4; }
+  .github:hover{ background:#000; }
+  .discord:hover{ background:#8c9eff; }
+
+  /* floating animation */
+  @keyframes float{
+    0%{ transform:translateY(0px); }
+    50%{ transform:translateY(-15px); }
+    100%{ transform:translateY(0px); }
+  }
+
+`;
+
